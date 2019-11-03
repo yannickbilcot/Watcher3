03abdc89cadf89c7801b91428792ebdc521182f2
Sergio Cambra <sergio@enpijama.es>
2019-11-03 15:02:02 +0100

    fix parsing torrentdownloads result when returns dict for title
    e.g. <title>The Merchant Of Venice / El mercader de venecia [bdrip 1280x544p x264] [h337 By<br />burnout]</title>

--- core/providers/torrent_modules/torrentdownloads.py
+++ core/providers/torrent_modules/torrentdownloads.py
@@ -72,7 +72,7 @@ def _parse(xml, imdbid):
             result['size'] = int(i['size'])
             result['status'] = 'Available'
             result['pubdate'] = None
-            result['title'] = i['title']
+            result['title'] = i['title']['content'] if isinstance(i['title'], dict) else i['title']
             result['imdbid'] = imdbid
             result['indexer'] = 'TorrentDownloads'
             result['info_link'] = 'http://www.torrentdownloads.me{}'.format(i['link'])
@@ -84,6 +84,7 @@ def _parse(xml, imdbid):
             result['download_client'] = None
             result['seeders'] = int(i['seeders'])
 
+            logging.debug('Found result: {}'.format(result))
             results.append(result)
         except Exception as e:
             logging.error('Error parsing TorrentDownloads XML.', exc_info=True)
