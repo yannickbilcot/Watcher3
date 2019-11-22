274ae68cf2ebcab4624d08670836ce9b8a894e90
ellnic <ellnic@ymail.com>
2019-11-22 23:37:51 +0000

    Update entrypoint.sh

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -5,6 +5,9 @@ set -e
 # Set user and group IDs if they were specified, else 0 (root)
 APPID="${APP_UID:=0}:${APP_GID:=0}"
 
+# Change ownership of config and app dirs
+chown -R $APPID /config /opt/watcher3 && chmod 775 /config /opt/watcher3
+
 # Check to see if the docker config folder contains default names, and rename and organise them.
 
 OLD_CFG=/config/config.cfg
@@ -14,7 +17,7 @@ if [ -f "$OLD_CFG" ]; then
         if [ -f /config/watcher.cfg ]; then
             echo "We seem to have successfully moved and renamed the old config, backing up original and tidying up."
                 if [ -z /config/backups ]; then
-                su-exec $APPID mkdir /config/backups
+                    su-exec $APPID mkdir /config/backups
                 fi
             su-exec $APPID cp /config/config.cfg /config/backups/config.cfg
             rm /config/config.cfg
@@ -29,7 +32,7 @@ if [ -f "$OLD_DB" ]; then
         if [ -f /config/db/database.sqlite ]; then
             echo "We seem to have successfully moved and renamed the old db, backing up original and tidying up."
                 if [ -z /config/backups ]; then
-                su-exec $APPID mkdir /config/backups
+                    su-exec $APPID mkdir /config/backups
                 fi
             su-exec $APPID cp /config/watcher.sqlite /config/backups/watcher.sqlite
             rm /config/watcher.sqlite
