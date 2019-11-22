4104306aa07ffe2024b6b565ee86c54d3fbd2519
ellnic <ellnic@ymail.com>
2019-11-22 22:58:47 +0000

    Added checks and backups to config/db rename

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -13,14 +13,30 @@ chown -R $APPID /config /opt/watcher3
 OLD_CFG=/config/config.cfg
 if [ -f "$OLD_CFG" ]; then
     echo "$OLD_CFG exists, renaming to watcher.cfg."
-    mv /config/config.cfg /config/watcher.cfg
+    cp /config/config.cfg /config/watcher.cfg
+        if [-f "/config/watcher.cfg" ]; then
+        echo "We seem to have successfully moved and renamed the old config, backing up original and tidying up."
+            if [-z "/config/backups" ]; then
+            mkdir /config/backups
+            fi
+        cp /config/config.cfg /config/backups/config.cfg
+        rm /config/config.cfg
+        fi
 fi
 
 OLD_DB=/config/watcher.sqlite
 if [ -f "$OLD_DB" ]; then
     echo "$OLD_DB exists, renaming to database.sqlite and moving into db folder."
     mkdir /config/db
-    mv /config/watcher.sqlite /config/db/database.sqlite
+    cp /config/watcher.sqlite /config/db/database.sqlite
+        if [-f "/config/db/database.sqlite"]; then
+            echo "We seem to have successfully moved and renamed the old db, backing up original and tidying up."
+                if [-z "/config/backups" ]; then
+                mkdir /config/backups
+                fi
+        cp /config/watcher.sqlite /config/backups/watcher.sqlite
+        rm /config/watcher.sqlite
+        fi   
 fi
 
 # Exec the CMD as the app user
